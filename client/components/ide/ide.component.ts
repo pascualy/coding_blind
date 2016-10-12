a21e09687f9711573420c179563d96aeb6194217
Gabriel Pascualy <pascualy@umich.edu>
2016-10-11 20:53:52 -0400

    unsure how to access skulpt library within angular controller

--- client/components/ide/ide.component.ts
+++ client/components/ide/ide.component.ts
@@ -1,6 +1,36 @@
 const angular = require('angular');
 
-export class IdeComponent {}
+
+export class IdeComponent {
+	$document; 
+	$window;
+	constructor($window, $document) {
+		this.$document = $document;
+		this.$window = $window;
+	}
+
+    outf = function(text) {
+        var output = document.getElementById("output");
+        text = text.replace(/</g, '&lt;');
+        output.innerHTML = output.innerHTML + text;
+    }
+
+    runit = function($window) {
+        var prog = document.getElementById("code").value;
+        var output = document.getElementById("output");
+        output.innerHTML = '';
+        $window.Sk.configure({output:outf});
+        try {
+            var module = Sk.importMainWithBody("<stdin>", false, prog);
+            var obj = module.tp$getattr('a');
+            var runMethod = obj.tp$getattr('run');
+            var ret = Sk.misceval.callsim(runMethod, 10);
+            alert(ret.v);
+        } catch (e) {
+            alert(e);
+        }
+    }
+}
 
 export default angular.module('directives.ide', [])
   .component('ide', {
