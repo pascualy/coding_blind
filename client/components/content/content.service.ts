78357f019a0e2ba2a0ae367427ac4a4c83c57d2f
mandhirajsingh <mndhrj@umich.edu>
2016-10-18 21:32:59 -0400

    updated lesson 1.1

--- client/components/content/content.service.ts
+++ client/components/content/content.service.ts
@@ -5,35 +5,25 @@ const angular = require('angular');
 export function ContentService($window) {
   'ngInject';
   var Content = [ {
-		"section" : '1.1',
-		"title" : 'Math in Python',
-		"header" : 'To the right is the Python interpreter. Any time you see this console to the right you can type in some Python code and hit enter and that code will run.\n\n Give it a try by typing in the year 2016 and subtracting out the year you were born. You should get your age (depending on if you\'ve had your birthday already, that is). For example, George Washington was born in 1732:',
-		"hints" : ['hint1: think about how you would use a calculator to complete this challenge','hint2: try and use your brain. that may help'],
-		"body" : 'blah blah blah blah ablah balha balahbalhsdhl asdlhf asdlhf',
-		"example" : "2016 - 1799",
-		"commands" : [ {
-						"command":"2016 - 1732",
-						"output":"284"
-					},
-					{
-						"command":"2016 - 1732",
-						"output":"284"
-					}]
-		},
-		{
-		"section" : '1.2',
-		"title" : 'Math in Python',
-		"header" : 'To the right is the Python interpreter. Any time you see this console to the right you can type in some Python code and hit enter and that code will run.\n\n Give it a try by typing in the year 2016 and subtracting out the year you were born. You should get your age (depending on if you\'ve had your birthday already, that is). For example, George Washington was born in 1732:',
-		"hints" : ['hint1: think about how you would use a calculator to complete this challenge','hint2: try and use your brain. that may help'],
-		"body" : 'blah blah blah blah ablah balha balahbalhsdhl asdlhf asdlhf',
-		"example" : "2016 - 1799",
-		"commands" : [ {
-						"command":"2016 - 1732",
-						"output":"284"
-					},
-					{
-						"command":"2016 - 1732",
-						"output":"284"
+		"lesson" : '1.1',
+		"title" : 'Python Basics',
+		"header" : '',
+		"hints" : ['Press alt R to run the code'],
+		"body" : `Let's start with a simple math problem! Programming languages are excellent	
+					for doing math because of their incredible speed. You can use the plus, dash, asterisk, or slash keys to do the 
+					four basic math operations: addition, subtraction, multiplication, and division.
+					Let's say we wanted to know what ten times twenty was. We could simply type in 10 * 20 and press enter.
+					However, let's say we were doing a long math problem and 10 * 20 was one of our intermediate steps. 
+					We could assign the result of 10 * 20 to a variable and then print to the screen. Think of a variable
+					as a holding place for piece of information where we can change what that piece of information is over time.
+					We can call this variable whatever we want, but in this problem, let's just call it the word result.
+					The 2 lines of code for this problem have already been written for you in the coding area. 
+					They read "result = 10 * 20 and print result." The first line assigns 10 * 20 to result, and the second line
+					prints the value of result to the screen. Simply press alt R to run the code.`
+		"example" : "",
+		"commands" : [{
+						"command":"result = 10*20\nprint result",
+						"output":"200"
 					}]
 		}];
 
